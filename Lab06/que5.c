7f9b69622bbad0323d76081280d2bd77129781cc
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2023-10-09 16:32:57 +0500

    Add files via upload

---
+++ Lab06/que5.c
@@ -0,0 +1,22 @@
+/**
+ * Programmer: Layyana Junaid
+ * Desc: Question 5 
+ * Date: 9/10/23
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+    int i=0;
+    while ( i <= 6 ) {
+      for (int j = 0; j < i + 1; j++) {
+      printf("%d ", i);
+    }
+     i++;
+     printf("\n");
+     
+        }  
+
+} // end main 
+ 
