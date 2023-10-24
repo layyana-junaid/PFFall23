1046d8c1d5f6f273cb4b38527cbc595321b4318e
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2023-10-24 09:32:04 +0500

    Add files via upload

---
+++ Lab08/q1.cpp
@@ -0,0 +1,33 @@
+
+
+#include <stdio.h>
+
+// Function to swap integers
+void swap( int*n1, int*n2 ) {
+	int temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+ int main () {
+ 	int num1; // number 1
+ 	int num2; // number 2
+ 	
+ 	printf("Enter the first integer: ");
+ 	scanf("%d",&num1);
+ 	printf("Enter the second integer: ");
+ 	scanf("%d",&num2);
+ 	
+ 	// displaying original format of numbers
+ 	
+ 	printf("Original placement Number 1: %d and Number 2: %d\n",num1,num2);
+    
+	// swaping the numbers
+	 
+	swap(&num1,&num2);
+	
+	printf("Swaped placement Number 1: %d and Number 2: %d\n",num1,num2);
+	
+    return 0;
+ 	
+ } // end main
+
