c18538ed43f3f5534dc758c4b1320f82beaa81fa
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2023-10-24 10:45:57 +0500

    Add files via upload

---
+++ Lab08/q4.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cstring>
+
+int main () {
+	 char password[100];
+	 printf("Enter your password: ");
+	 scanf("%s", password);
+	 
+	 // checking the length of the password
+	 if (strlen(password) < 8) {
+	 	printf("Incorrect password!! It must be atleast 8 characters long.\n");
+	 	return 1;
+	 }
+	  
+	  // if it meets the length requirment 
+	  char storedpassword[] = "Secure123";
+	  if(strcmp(password, storedpassword) == 0) {
+	  	printf("Login Successful. Welcome!\n");
+	}
+	   else {
+	   	printf("Login Failed. Incorrect Password!\n");
+
+	   }
+	   
+	return 0;
+	
+} // end main
