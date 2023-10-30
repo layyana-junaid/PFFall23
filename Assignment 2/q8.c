abaa6b85a0c8a9fd86a63e549ef4d981111c655f
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2023-10-30 21:29:08 +0500

    Add files via upload

---
+++ Assignment 2/q8.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int calculate_persistence(int num) {
+    int persistence = 0;
+    
+    while (num >= 10) {
+        int product = 1;
+        while (num > 0) {
+            product *= num % 10;
+            num /= 10;
+        }
+        num = product;
+        persistence++;
+    }
+    
+    return persistence;
+}
+
+int main() {
+    int n;
+    while (1) {
+        printf("Enter a positive integer (or EOF to exit): ");
+        int result = scanf("%d", &n);
+        printf("-----------------------------------------\n");
+        if (result == EOF) {
+            break; // Exit the loop on EOF
+        } else if (result == 0 || n < 1) {
+            printf("Invalid input. Please enter a positive integer.\n");
+            printf("-----------------------------------------------------\n");
+            while (getchar() != '\n'); // Clear the input buffer
+            continue;
+        }
+        
+        int persistence = calculate_persistence(n);
+        printf("Persistence of %d is %d\n", n, persistence);
+        printf("------------------------------------\n");
+    }
+    
+    printf("Program terminated.\n");
+    printf("---------------------------\n");
+    return 0;
+    
+} // end main 
+
