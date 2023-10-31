5b12a8b7a07246295c626f6e51be80d222b54d96
layyana-junaid <142867946+layyana-junaid@users.noreply.github.com>
2023-10-31 08:45:47 +0500

    Add files via upload

---
+++ Lab09/task2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// decalaration of reverse function 
+void reverse(int *arr, int size){
+	int i; // counter for 'for loop'
+	printf("The elements of the array in reverse order are: ");
+    for (i = 10 - 1; i >= 0; i--) {
+	printf("%d ", arr[i]);
+    }
+}
+int main (void) {
+    int arr[10];
+    int i; // counter for 'for loop'
+    int s;
+    printf("Enter the size of array: ");
+    scanf("%d",&s);
+    for ( i=0; i < 10; i++ ) {
+    printf("Enter the elements %d: ", i + 1);
+    scanf("%d", &arr[i]);
+    }
+    reverse(arr,s);
+} // end main
+                  
